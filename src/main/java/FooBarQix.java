d212b4b4c0fed57c0773ff517632d7341e44a064
youribonnaffe <youribm@gmail.com>
2011-12-03 17:36:40 +0100

    in real life I would have stopped on previous commit, since this is for fun we can abstract the rules a bit

--- src/main/java/FooBarQix.java
+++ src/main/java/FooBarQix.java
@@ -2,25 +2,27 @@ public class FooBarQix {
 
     static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
-    static final int THREE = 3;
-    static final String THREE_AS_TEXT = String.valueOf(THREE);
-    static final String FOO = "Foo";
+    // rules of foobarqix as arrays
+    static final int[] NUMBERS = {3, 5, 7};
+    static final String[] REPLACEMENTS = {"Foo", "Bar", "Qix"};
 
-    static final int FIVE = 5;
-    static final String FIVE_AS_TEXT = String.valueOf(FIVE);
-    static final String BAR = "Bar";
+    static final String[] NUMBERS_AS_TEXT;
 
-    static final int SEVEN = 7;
-    static final String SEVEN_AS_TEXT = String.valueOf(SEVEN);
-    static final String QIX = "Qix";
-
-    static final int OUTPUT_LOWER_LIMIT = 1;
-    static final int OUTPUT_UPPER_LIMIT = 100;
+    static {
+        NUMBERS_AS_TEXT = new String[NUMBERS.length];
+        for (int i = 0; i < NUMBERS.length; i++) {
+            NUMBERS_AS_TEXT[i] = String.valueOf(NUMBERS[i]);
+        }
+    }
 
     public static String oneToAHundred() {
+        return computeWithLimits(1, 100);
+    }
+
+    public static String computeWithLimits(int lowerLimit, int upperLimit) {
         String output = "";
-        for (int i = OUTPUT_LOWER_LIMIT; i <= OUTPUT_UPPER_LIMIT; i++) {
-            output += compute(i) + (i == OUTPUT_UPPER_LIMIT ? "" : LINE_SEPARATOR);
+        for (int i = lowerLimit; i <= upperLimit; i++) {
+            output += compute(i) + (i == upperLimit ? "" : LINE_SEPARATOR);
         }
         return output;
     }
@@ -36,27 +38,23 @@ public class FooBarQix {
         return result;
     }
 
-    private static String computeDivisionRules(int number) {
+    private static String computeDivisionRules(int currentNumber) {
         String result = "";
-        if (number % THREE == 0) {
-            result += FOO;
-        }
-        if (number % FIVE == 0) {
-            result += BAR;
-        }
-        if (number % SEVEN == 0) {
-            result += QIX;
+        for (int i = 0; i < NUMBERS.length; i++) {
+            if (currentNumber % NUMBERS[i] == 0) {
+                result += REPLACEMENTS[i];
+            }
         }
         return result;
     }
 
     private static String computeContentRules(int number) {
         String numberAsText = String.valueOf(number);
-        String numberAsTextReplaced = numberAsText.replaceAll(THREE_AS_TEXT, FOO);
-        numberAsTextReplaced = numberAsTextReplaced.replaceAll(FIVE_AS_TEXT, BAR);
-        numberAsTextReplaced = numberAsTextReplaced.replaceAll(SEVEN_AS_TEXT, QIX);
-        numberAsTextReplaced = removeDigitLeft(numberAsTextReplaced);
-        return numberAsTextReplaced;
+        String numberAsTextReplaced = numberAsText;
+        for (int i = 0; i < NUMBERS.length; i++) {
+            numberAsTextReplaced = numberAsTextReplaced.replaceAll(NUMBERS_AS_TEXT[i], REPLACEMENTS[i]);
+        }
+        return removeDigitLeft(numberAsTextReplaced);
     }
 
     private static String removeDigitLeft(String numberAsTextReplaced) {
--- src/test/java/FooBarQixTest.java
+++ src/test/java/FooBarQixTest.java
@@ -1,40 +1,54 @@
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class FooBarQixTest {
+    @Test
+    public void testPreConditionsOnNumbers() {
+        int[] sortedNumbers = Arrays.copyOf(FooBarQix.NUMBERS, FooBarQix.NUMBERS.length);
+        Arrays.sort(sortedNumbers);
+        Assert.assertArrayEquals(FooBarQix.NUMBERS, sortedNumbers);
+        assertArrayEquals(FooBarQix.NUMBERS_AS_TEXT, sortedNumbers);
+
+        assertEquals(FooBarQix.NUMBERS.length, FooBarQix.NUMBERS_AS_TEXT.length);
+        assertEquals(FooBarQix.NUMBERS.length, FooBarQix.REPLACEMENTS.length);
+    }
+
+    private void assertArrayEquals(String[] texts, int[] ints) {
+        String[] intsAsText = new String[ints.length];
+        for (int i = 0; i < ints.length; i++) {
+            intsAsText[i] = String.valueOf(ints[i]);
+        }
+        Assert.assertArrayEquals(texts, intsAsText);
+    }
 
     @Test
     public void testRulesAreRespected() throws Exception {
         String output = FooBarQix.oneToAHundred();
-        for (int lineNumber = FooBarQix.OUTPUT_LOWER_LIMIT; lineNumber <= FooBarQix.OUTPUT_UPPER_LIMIT; lineNumber++) {
+        for (int lineNumber = 1; lineNumber <= 100; lineNumber++) {
             assertDivisionRuleIsRespected(lineNumber, LineUtils.line(lineNumber, output));
             assertContentRuleIsRespected(lineNumber, LineUtils.line(lineNumber, output));
         }
     }
 
     private void assertDivisionRuleIsRespected(int lineNumber, String line) {
-        if (lineNumber % FooBarQix.THREE == 0) {
-            assertLineContent(lineNumber, line, FooBarQix.FOO);
-        }
-        if (lineNumber % FooBarQix.FIVE == 0) {
-            assertLineContent(lineNumber, line, FooBarQix.BAR);
-        }
-        if (lineNumber % FooBarQix.SEVEN == 0) {
-            assertLineContent(lineNumber, line, FooBarQix.QIX);
+        for (int i = 0; i < FooBarQix.NUMBERS.length; i++) {
+            if (lineNumber % FooBarQix.NUMBERS[i] == 0) {
+                assertLineContent(lineNumber, line, FooBarQix.REPLACEMENTS[i]);
+            }
         }
     }
 
     private void assertContentRuleIsRespected(int lineNumber, String line) {
         String lineNumberAsText = String.valueOf(lineNumber);
-        if (lineNumberAsText.contains(FooBarQix.THREE_AS_TEXT)) {
-            assertLineContent(lineNumber, line, FooBarQix.FOO);
-        }
-        if (lineNumberAsText.contains(FooBarQix.FIVE_AS_TEXT)) {
-            assertLineContent(lineNumber, line, FooBarQix.BAR);
-        }
-        if (lineNumberAsText.contains(FooBarQix.SEVEN_AS_TEXT)) {
-            assertLineContent(lineNumber, line, FooBarQix.QIX);
+        for (int i = 0; i < FooBarQix.NUMBERS.length; i++) {
+            if (lineNumberAsText.contains(FooBarQix.NUMBERS_AS_TEXT[i])) {
+                assertLineContent(lineNumber, line, FooBarQix.REPLACEMENTS[i]);
+            }
         }
     }
 
